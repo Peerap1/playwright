509c05da592ab9a5827f96181a51e808d114a4b1
Peerapol Tapeng <peerapol.tap@ttbbank.com>
2022-11-18 17:46:02 +0700

    expect

--- tests/test_demo.spec.js
+++ tests/test_demo.spec.js
@@ -1,28 +1,26 @@
 // npx playwright test /tests/test_demo.spec.js --project chromium --headed
 const { test, expect } = require('@playwright/test');
 
-test('Login Demo 1', async ({page}) => {
+test('Login Demo 1', async ({ page }) => {
 
     await page.goto('https://demo.applitools.com/')
-    
+
     await page.locator('[placeholder="Enter your username"]').fill('peerapol')
     await page.locator('[placeholder="Enter your password"]').fill('pwd1234')
-    await page.waitForSelector('text=Sign in', {timeout:4000})
+    await page.waitForSelector('text=Sign in', { timeout: 4000 })
     await page.locator('text=Sign in').click()
-    
-    await page.pause()
+    await page.locator('xpath=//*[text()="Lending"]').click()
+    await page.locator('text=Make Payment').click()
+    await page.locator('text=Make Payment').click()
 
     await expect(page.locator('text=Make Payment')).toBeVisible()
-    await page.locator('text=Make Payment').click()
-    await page.locator('text=Make Payment').click()
-
     await expect(page.locator('text=Lending')).toBeVisible()
     await expect(page.getByRole('link', { name: '  Loans' })).toBeVisible()
     await expect(page.locator('text=Lending')).toBeVisible()
     await expect(page.getByRole('heading', { name: 'Financial Overview' })).toBeVisible()
+    await expect(page.locator('text=ramdom')).not.toBeVisible()
+    await expect(page.locator('xpath=//*[text()="Lending"]')).toBeVisible()
 
     await page.close()
 
 });
-
-
