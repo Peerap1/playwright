a6a1af89a4c9f378e5ff2ce5e95e43d4855a54d3
OLePee <>
2022-11-19 22:10:25 +0700

    sec2

--- tests/demo_js/second.js
+++ tests/demo_js/second.js
@@ -1,11 +1,11 @@
 // node tests/demo_js/second.js
 
-function sayHello (){
+function sayHello() {
     console.log("คำนวณ")
 }
 sayHello()
 
-function toCelsius(){
+function toCelsius() {
     let fahrenheit = Math.floor(Math.random() * 200)
     console.log(fahrenheit)
     let value = (fahrenheit - 32) * 5 / 9
@@ -14,16 +14,46 @@ function toCelsius(){
 toCelsius()
 
 // return
-function toCelsius2(fahrenheit){
+function toCelsius2(fahrenheit) {
     let value = (fahrenheit - 32) * 5 / 9
     return (value.toFixed(2))
 }
 console.log(toCelsius2(200))
 
 // Send agument
-function seyHello(username){
+function seyHello(username) {
     return "My name is " + username
-    
+
 }
 let names = 'OLe Peerapol'
 console.log(seyHello(names))
+
+// Sec 2
+var myCar = {
+    brand: "Honda",
+    model: "City",
+    cloor: "Black",
+    fullName: function () {
+        return this.brand + " " + this.model
+    }
+};
+console.log(myCar.fullName());
+
+// // log index in list
+// for (var x in myCar) {
+//     console.log(myCar[x])
+// };
+
+// var cars = ["Honda", "City", "Black"]
+// for (var y of cars) {
+//     console.log(y)
+// };
+
+
+var toCelsius10 = () => {
+    let fahrenheit = Math.floor(Math.random() * 200)
+    console.log(fahrenheit)
+    let value = (fahrenheit - 32) * 5 / 9
+    console.log(value.toFixed(2))
+}
+toCelsius10()
