ad3a6c0c7e31fe0d2e8afc1d20a7d1426f27503d
Peerapol Tapeng <peerapol.tap@ttbbank.com>
2022-11-20 12:44:15 +0700

    ts

---
+++ tests/demo_js/ts_test.ts
@@ -0,0 +1,6 @@
+// sudo npm install -g typescript
+// sudo npm install -g ts-node typescript   // '@type/node'
+// tsc tests/demo_js/ts01.ts
+// ts-node tests/demo_js/ts01.ts
+
+console.log("OLe Peerapol");
